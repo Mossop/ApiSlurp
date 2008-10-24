a749d1ad105cdf5b4852a59abe4daf7e86904c08
Dave Townsend <dtownsend@oxymoronical.com>
2008-10-24 01:41:41 -0700

    Add local config and error handling

--- webapp/setup.php
+++ webapp/setup.php
@@ -1,16 +1,39 @@
 <?php
+if (!is_dir('compiled') || !is_dir('cache') || !is_file('smarty/Smarty.class.php')) {
+  print 'Web-app not properly set up. Create compile and cache directories and install smarty.';
+  exit;
+}
+
 require_once('smarty/Smarty.class.php');
+require_once('config.php');
+
 $smarty = new Smarty();
 
 $smarty->template_dir         = 'templates/';
 $smarty->compile_dir          = 'compiled/';
 $smarty->cache_dir            = 'cache/';
-$smarty->caching              = 1;
-$smarty->cache_modified_check = 1;
+$smarty->cache_modified_check = true;
 
-$smarty->assign('ROOT', '/experiments/apidocs');
+$smarty->assign('ROOT', $webroot);
 
-$db = new SQLiteDatabase('api.sqlite');
+if (!is_file($dbpath)) {
+  $smarty->assign('error', 'No database');
+  $smarty->assign('message', 'The API database was not found.');
+  $smarty->display('error.tpl');
+  exit;
+}
+
+try {
+  $db = new SQLiteDatabase($dbpath);
+}
+catch (Exception $e) {
+  $smarty->assign('error', 'Corrupt database');
+  $smarty->assign('message', 'The database could not be opened: ' . $e->getMessage());
+  $smarty->display('error.tpl');
+  exit;
+}
+
+$smarty->caching = true;
 
 function sqlesc($str) {
   return sqlite_escape_string($str);
