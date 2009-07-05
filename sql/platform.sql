ebf844fa3c905370702893db41baa10a35109c99
Dave Townsend <dtownsend@oxymoronical.com>
2009-07-05 15:35:57 +0100

    Switch to fixtures rather than initial sql

--- sql/platform.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO xpcomref_platform (codename, name) VALUES ("linux", "Linux");
-INSERT INTO xpcomref_platform (codename, name) VALUES ("darwin", "Mac OSX");
-INSERT INTO xpcomref_platform (codename, name) VALUES ("winnt", "Windows");
